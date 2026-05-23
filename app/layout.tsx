b3aa188fe55a8f3a8500098deed7238600096537
garibcatastrofe <ramsesgaribflorescuen@gmail.com>
2026-05-23 15:56:15 -0700

    feat: :sparkles: Implementación de tipografías

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bebas_Neue, DM_Sans } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const bebasNeue = Bebas_Neue({
+  variable: "--font-bebas-neue",
   subsets: ["latin"],
+  weight: ["400"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -27,7 +29,7 @@ export default function RootLayout({
   return (
     <html
       lang="es"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${bebasNeue.variable} ${dmSans.variable} h-full antialiased`}
       suppressHydrationWarning
     >
       <body>
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import { ThemeToggle } from "@/components/theme/ThemeToogle";
 export default function Home() {
   return (
     <div className="max-h-dvh h-dvh w-full">
-      <div className="w-full h-full p-4 flex items-center justify-center flex-col gap-2">
-        <h1 className="text-7xl font-bold text-primary italic">
-          Paleta de colores
-        </h1>
+      <div className="w-full h-full p-4 flex items-center justify-center flex-col gap-4">
+        <h2 className="text-4xl font-bold text-secondary italic">
+          4Sports
+        </h2>
+        <h1 className="text-6xl font-bold text-primary font-bebas">Paleta de colores</h1>
+
         <p className="text-muted">
           Cambia el tema con el botón en la esquina inferior izquierda
         </p>
